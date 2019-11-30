69a0d2c96691852183e1c2c7698ea39be173a042
Jairo <jjotta158@outlook.com>
2019-11-30 01:11:20 -0300

    Pegando um máximo de sentenças e usando o watsom pra pegar as keywords

--- robots/orchestrator.js
+++ robots/orchestrator.js
@@ -8,7 +8,7 @@ async function start()
     const orchestrator = {}; // Object that is the orchestrator
     robots.userInput(orchestrator); //robot to catch a user input.
     await robots.text(orchestrator);
-    console.log(orchestrator.sourceContentSanitized);
+    console.log(orchestrator);
 }
 
 start();
--- robots/userInput.js
+++ robots/userInput.js
@@ -3,18 +3,24 @@ const readLine = require("readline-sync");
 module.exports = (orchestrator) => {
     orchestrator.searchTerm = askSearchTerm()
     orchestrator.prefix = askPrefix();
+    orchestrator.maximumSentences = maxSentences();
 
     function askSearchTerm ()
     {
-      return readLine.question("Type a Wikipedia seach term: ");
+        return readLine.question("Type a Wikipedia seach term: ");
     }
 
     function askPrefix()
     {
-      const prefixes = ['Who is', 'what is', 'The History of'];
-      const selectedPrefixIndex = readLine.keyInSelect(prefixes);
-      const selectedPrefixText = prefixes[selectedPrefixIndex];
-      return selectedPrefixText;
+        const prefixes = ['Who is', 'what is', 'The History of'];
+        const selectedPrefixIndex = readLine.keyInSelect(prefixes);
+        const selectedPrefixText = prefixes[selectedPrefixIndex];
+        return selectedPrefixText;
+    }
+
+    function maxSentences()
+    {
+        return readLine.question("Type the max of sentences that you want ");
     }
     return orchestrator;
 }

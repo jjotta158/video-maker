d0a3da5c88e6236cb65ff03c669a5aee9ccf05a1
Jairo <jjotta158@outlook.com>
2019-12-04 16:40:45 -0300

    adição e configuração do robo de imagens ao projeto

---
+++ robots/image.js
@@ -0,0 +1,9 @@
+const state = require('./state.js');
+
+
+async function robot()
+{
+    const content = state.load();
+}
+
+module.exports = robot();
--- robots/orchestrator.js
+++ robots/orchestrator.js
@@ -1,6 +1,8 @@
 const robots = {
     userInput:require("./userInput"),
-    text:require("./text")
+    text:require("./text"),
+    state: require('./fileSync'),
+    image: require('./image')
 }
 
 async function start()
@@ -8,6 +10,7 @@ async function start()
     const orchestrator = {}; // Object that is the orchestrator
     robots.userInput(orchestrator); //robot to catch a user input.
     await robots.text();
+    await robots.image();
 }
 
 start();
